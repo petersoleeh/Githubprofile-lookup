4b96d25316bf1e197daea917c00dcb21330c3a71
peter soleeh <petersoleeh@gmail.com>
2017-09-08 17:30:46 +0300

    add users

--- build/js/app.js
+++ build/js/app.js
@@ -7,7 +7,9 @@ function User() {}
 
 User.prototype.getUser = function(githubuser){
   $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response){
+
     console.log(response);
+
   }).fail(function(error){
     $('#error').text(error.responseJSON.message);
   });
--- js/user.js
+++ js/user.js
@@ -4,7 +4,9 @@ function User() {}
 
 User.prototype.getUser = function(githubuser){
   $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response){
+
     console.log(response);
+
   }).fail(function(error){
     $('#error').text(error.responseJSON.message);
   });
