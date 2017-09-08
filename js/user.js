dd6ef7054033bb41792c19e48529c99ac25a71df
peter soleeh <petersoleeh@gmail.com>
2017-09-08 17:11:42 +0300

    different users add different users

--- js/user.js
+++ js/user.js
@@ -3,7 +3,7 @@ var apiKey = require('./../.env').apiKey;
 function User() {}
 
 User.prototype.getUser = function(githubuser){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     $('#error').text(error.responseJSON.message);
