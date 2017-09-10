1dee9288cae7dd71426bfffa7a7dd9d3a16195a1
peter soleeh <petersoleeh@gmail.com>
2017-09-10 12:02:11 +0300

    appending the repos col for appending the avatar and repos

--- build/js/app.js
+++ build/js/app.js
@@ -7,22 +7,16 @@ function User() {}
 
 User.prototype.getUser = function(githubuser){
   $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response){
-  	
-    console.log(response);
-    $('.img-avatar').html('<h2 id="user-name">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
-  
-
+  	 console.log(response);
+    $('.username').html('<h2 id="user-name">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
   }).fail(function(error){
     // $('#error').text(githubuser + ' could not be found, please enter a valid username');
   
 }); 
-};
-User.prototype.getRepos = function(githubuser){
   $.get('https://api.github.com/users/' + githubuser + '/repos?access_token=' + apiKey).then(function(response){
   	for (var n = 0; n <= response.length; n++)
       $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name +'</p><p>' + response[n].description + '</p></a></li>');
-    // console.log(response);
-   
+    // console.log(response);  
   }).fail(function(error){
   
   });
