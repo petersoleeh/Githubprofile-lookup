0b7d22608664e8cf988eff8933f1d1add94e910a
peter soleeh <petersoleeh@gmail.com>
2017-09-10 12:17:58 +0300

    col updates

--- js/user.js
+++ js/user.js
@@ -5,13 +5,15 @@ function User() {}
 User.prototype.getUser = function(githubuser){
   $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response){
   	 console.log(response);
-    $('.username').html('<h2 id="user-name">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
+    $('.username').html('<h2 id="githubuser">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
   }).fail(function(error){
-    // $('#error').text(githubuser + ' could not be found, please enter a valid username');
+    $('error').empty();
+    $('#error').text(githubuser + ' could not be found, please enter a valid username');
   
 }); 
   $.get('https://api.github.com/users/' + githubuser + '/repos?access_token=' + apiKey).then(function(response){
   	for (var n = 0; n <= response.length; n++)
+      // $('#repo').empty();
       $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name +'</p><p>' + response[n].description + '</p></a></li>');
     // console.log(response);  
   }).fail(function(error){
