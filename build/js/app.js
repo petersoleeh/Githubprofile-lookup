15a2e4fdf9b8033ab49a6be76eb492f38e86a79c
peter soleeh <petersoleeh@gmail.com>
2017-09-10 13:42:23 +0300

    build error change

--- build/js/app.js
+++ build/js/app.js
@@ -36,8 +36,8 @@ var User =  require('./../js/user.js').userModule;
 $(document).ready(function () {
 	
 	var currentUserObject = new User();
-	$('#searchUser').click(function (e) {
-		e.preventDefault();
+	$('#searchUser').click(function (event) {
+		event.preventDefault();
 		var githubuser = $('#github-username').val();
 		$('#github-username').val("");
 		$('#repo').html("");
