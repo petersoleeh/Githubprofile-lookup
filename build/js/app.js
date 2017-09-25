9d1e96683a06e61f1f06cd04c6749ac8bd65a9e9
peter soleeh <petersoleeh@gmail.com>
2017-09-25 11:00:42 +0300

    insert borders

--- build/js/app.js
+++ build/js/app.js
@@ -1,63 +1,94 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-exports.apiKey = "84127e6dc740402031a4458e635b6e7efdc278ef"
-},{}],2:[function(require,module,exports){
-var apiKey = require('./../.env').apiKey;
-
-function User() {}
-
-User.prototype.getUser = function(githubuser){
-  $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response){
-  	 console.log(response);
-    $('.username').html('<h2 id="githubuser">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
-  }).fail(function(error){
-    
-    $('#error').text(githubuser + ' could not be found, please enter a valid username');
-  
-}); 
-  $.get('https://api.github.com/users/' + githubuser + '/repos?access_token=' + apiKey).then(function(response){
-  	for (var n = 0; n <= response.length; n++)
-      // $('#repo').empty();
-      if (response[n].description === null) {
-      $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>Oops no description here!!!! </p></a></li>');
-    } else {
-      $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name +'</p><p>' + response[n].description + '</p></a></li>');
+(function e(t, n, r) {
+  function s(o, u) {
+    if (!n[o]) {
+      if (!t[o]) {
+        var a = typeof require == "function" && require;
+        if (!u && a) return a(o, !0);
+        if (i) return i(o, !0);
+        var f = new Error("Cannot find module '" + o + "'");
+        throw f.code = "MODULE_NOT_FOUND", f
+      }
+      var l = n[o] = {
+        exports: {}
+      };
+      t[o][0].call(l.exports, function(e) {
+        var n = t[o][1][e];
+        return s(n ? n : e)
+      }, l, l.exports, e, t, n, r)
     }
-    // console.log(response);  
-  }).fail(function(error){
-  
-  });
-};
+    return n[o].exports
+  }
+  var i = typeof require == "function" && require;
+  for (var o = 0; o < r.length; o++) s(r[o]);
+  return s
+})({
+  1: [function(require, module, exports) {
+    exports.apiKey = "84127e6dc740402031a4458e635b6e7efdc278ef"
+  }, {}],
+  2: [function(require, module, exports) {
+    var apiKey = require('./../.env').apiKey;
+
+    function User() {}
+
+    User.prototype.getUser = function(githubuser) {
+      $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response) {
+        console.log(response);
+        $('.username').html('<h2 id="githubuser">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
+      }).fail(function(error) {
+
+        $('#error').text(githubuser + ' could not be found, please enter a valid username');
+
+      });
+      $.get('https://api.github.com/users/' + githubuser + '/repos?access_token=' + apiKey).then(function(response) {
+        for (var n = 0; n <= response.length; n++)
+          // $('#repo').empty();
+          if (response[n].description === null) {
+            $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>Oops no description here!!!! </p></a></li>');
+          } else {
+            $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>' + response[n].description + '</p></a></li>');
+          }
+        // console.log(response);
+      }).fail(function(error) {
+
+      });
+    };
 
 
-exports.userModule = User;
+    exports.userModule = User;
 
 
-},{"./../.env":1}],3:[function(require,module,exports){
-var User =  require('./../js/user.js').userModule;
-
-
-
-$(document).ready(function () {
-	
-	var currentUserObject = new User();
-	$('#searchUser').click(function (event) {
-		event.preventDefault();
-		var githubuser = $('#github-username').val();
-		$('#github-username').val("");
-		$('#repo').empty("");
-		$('#error').empty("");
-		$('.username').empty("");
-		// $('.username').text(githubuser);
-		currentUserObject.getUser(githubuser);
-	});
-});
+  }, {
+    "./../.env": 1
+  }],
+  3: [function(require, module, exports) {
+    var User = require('./../js/user.js').userModule;
 
 
 
+    $(document).ready(function() {
+
+      var currentUserObject = new User();
+      $('#searchUser').click(function(event) {
+        event.preventDefault();
+        var githubuser = $('#github-username').val();
+        $('#github-username').val("");
+        $('#repo').empty("");
+        $('#error').empty("");
+        $('.username').empty("");
+        // $('.username').text(githubuser);
+        currentUserObject.getUser(githubuser);
+      });
+    });
 
 
 
 
 
 
-},{"./../js/user.js":2}]},{},[3]);
+
+
+
+  }, {
+    "./../js/user.js": 2
+  }]
+}, {}, [3]);
--- js/user.js
+++ js/user.js
@@ -2,29 +2,28 @@ var apiKey = require('./../.env').apiKey;
 
 function User() {}
 
-User.prototype.getUser = function(githubuser){
-  $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response){
-  	 console.log(response);
+User.prototype.getUser = function(githubuser) {
+  $.get('https://api.github.com/users/' + githubuser + '? access_token=' + apiKey).then(function(response) {
+    console.log(response);
     $('.username').html('<h2 id="githubuser">' + response.name + '</h2><img id="user-avatarImage" src=' + response.avatar_url + '>');
-  }).fail(function(error){
-    
+  }).fail(function(error) {
+
     $('#error').text(githubuser + ' could not be found, please enter a valid username');
-  
-}); 
-  $.get('https://api.github.com/users/' + githubuser + '/repos?access_token=' + apiKey).then(function(response){
-  	for (var n = 0; n <= response.length; n++)
+
+  });
+  $.get('https://api.github.com/users/' + githubuser + '/repos?access_token=' + apiKey).then(function(response) {
+    for (var n = 0; n <= response.length; n++)
       // $('#repo').empty();
       if (response[n].description === null) {
-      $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>Oops no description here!!!! </p></a></li>');
-    } else {
-      $('#repo').append('<li><a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name +'</p><p>' + response[n].description + '</p></a></li>');
-    }
-    // console.log(response);  
-  }).fail(function(error){
-  
+        $('#repo').append('<a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>Oops no description here!!!! </p></a><');
+      } else {
+        $('#repo').append('<a target="_blank" href="' + response[n].html_url + '"><p>' + response[n].name + '</p><p>' + response[n].description + '</p></a>');
+      }
+    // console.log(response);
+  }).fail(function(error) {
+
   });
 };
 
 
 exports.userModule = User;
-
